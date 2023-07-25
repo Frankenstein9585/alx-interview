45031cc295bc219df878576eb996b076adbe56f6
Frankenstein9585 <donny9585@gmail.com>
2023-07-25 16:55:28 +0100

    Done task

---
+++ 0x00-pascal_traingle/0-pascal_triangle.py
@@ -0,0 +1,13 @@
+def pascal_triangle(n):
+    """This function returns Pascal's triangle"""
+    if n <= 0:
+        return []
+    pascal_list = [[1]]
+    while len(pascal_list) < n:
+        last_row = pascal_list[-1]
+        new_row = [1]
+        for i in range(1, len(last_row)):
+            new_row.append(last_row[i - 1] + last_row[i])
+        new_row.append(1)
+        pascal_list.append(new_row)
+    return pascal_list
