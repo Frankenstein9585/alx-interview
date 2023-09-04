6ea6d32cd15b10d8913872d43154a1b3313a7a2f
Frankenstein9585 <donny9585@gmail.com>
2023-09-04 02:09:56 +0100

    Modified

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -38,19 +38,15 @@ def solve_n_queens(n: int) -> list[list[int]]:
 
 
 try:
+    if len(sys.argv) != 2:
+        print('Usage: nqueens N\n')
+        sys.exit(1)
     args = int(sys.argv[1])
 except ValueError:
     print("N must be a number")
     sys.exit(1)
-except IndexError:
-    print('Usage: nqueens N')
-    sys.exit(1)
 
-if len(sys.argv) != 2:
-    print('Usage: nqueens N')
-    sys.exit(1)
-
-elif args < 4:
+if args < 4:
     print("N must be at least 4")
     sys.exit(1)
 
