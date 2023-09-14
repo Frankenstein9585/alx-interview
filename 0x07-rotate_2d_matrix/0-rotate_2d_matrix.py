70460d48774f92bf00306c23f4604f85efb50d83
Frankenstein9585 <donny9585@gmail.com>
2023-09-14 11:40:05 +0100

    Implemented rotate_2d_matrix(): Removed type annotation

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,7 +2,7 @@
 """Rotate 2D Matrix"""
 
 
-def rotate_2d_matrix(matrix: list[list[int]]) -> None:
+def rotate_2d_matrix(matrix):
     """Rotates a 2D Matrix in place"""
     copy = [row[:] for row in matrix]
     size = len(matrix)
